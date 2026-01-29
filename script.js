7ec9bdc13ef5c503722ae72af069a5f77807205e
miltonbanksy <miltonbanksy@gmail.com>
2026-01-29 14:19:23 +0900

    set up old school boiler

---
+++ script.js
@@ -0,0 +1,10 @@
+const btnGenerateCharacter = document.getElementById('btn-create-character');
+const displayCharacter = document.getElementById('display-character');
+
+function generateStats() {
+
+}
+
+btnGenerateCharacter.addEventListener('click', () => {
+    const stats = generateStats();
+});
